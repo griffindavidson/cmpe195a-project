bab43629ea7f0b0afacfdb3186d2e0d1b0877904
Griffin Davidson <69946180+griffindavidson@users.noreply.github.com>
2024-10-12 12:08:12 -0700

    add homepage and started article page

---
+++ src/app/article/page.js
@@ -0,0 +1,7 @@
+export default function article() {
+    return (
+        <div class='container'>
+            <h1 style={{textAlign: 'start'}}>Article Name</h1>
+        </div>
+    )
+}
--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,26 @@
 import Image from "next/image";
-import styles from "./page.module.css";
+import styles from "./home.css";
 
 export default function Home() {
   return (
-    <div className="home">
-      <p>CMPE 195 Hollistic Cancer Reserch App</p>
+    <div>
+      <h1>Suggested Articles</h1>
+      <div class="suggested-container">
+        <div class="item">Item 1</div>
+        <div class="item">Item 2</div>
+        <div class="item">Item 3</div>
+        <div class="item">Item 4</div>
+      </div>
+      <h1>Saved Articles</h1>
+      <div class="saved-container">
+        <div class="item">Item 1</div>
+        <div class="item">Item 2</div>
+        <div class="item">Item 3</div>
+      </div>
+      <h1>Search</h1>
+      <div class="search-container">
+        <input type="text" placeholder="search" class="search-textfield"/>
+      </div>
     </div>
   );
 };
