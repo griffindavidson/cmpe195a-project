f8ba594501755b57033f5470a5d1ad17de84ee7a
Griffin Davidson <69946180+griffindavidson@users.noreply.github.com>
2025-01-19 17:23:21 -0800

    started working on fetching from python and displaying raw data

--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,37 @@
+'use client'
+
+import { useEffect, useState } from "react";
 import Image from "next/image";
 import styles from "./home.css";
 
 export default function Home() {
+
+  const [articles, setArticles] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    const fetchArticles = async () => {
+      try {
+        const res = await fetch('/api/fetchPubMedData');
+        if (!res.ok) {
+          throw new Error('Failed to fetch data');
+        }
+        const data = await res.json();
+        setArticles(data);
+      } catch (err) {
+        setError(err.message);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchArticles();
+  }, []);
+
+  if (loading) return <div>Loading...</div>;
+  if (error) return <div>Error: {error}</div>;
+
   return (
     <div>
       <h1>Suggested Articles</h1>
@@ -36,6 +66,21 @@ export default function Home() {
           <div className="search-result-title">Article Title</div>
         </div>
         
+      </div>
+      <div class="test">
+        <h1>PubMed Articles</h1>
+        <ul>
+          {articles.map((article, index) => (
+            <li key={index}>
+              <h2>{article.title}</h2>
+              <p><strong>Authors:</strong> {article.authors.join(', ')}</p>
+              <p><strong>Source:</strong> {article.source}</p>
+              <p><strong>Journal:</strong> {article.journal}</p>
+              <p><strong>Volume:</strong> {article.volume}</p>
+              <p><strong>Abstract:</strong> {article.abstract}</p>
+            </li>
+          ))}
+        </ul>
       </div>
     </div>
   );
